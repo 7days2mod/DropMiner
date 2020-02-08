60c09993ab7d5d4979fac6e8d394f146abc575bc
StompyNZ <stompynz@gmail.com>
2020-02-09 11:33:03 +1300

    cleanup

--- DropMiner/src/API.cs
+++ DropMiner/src/API.cs
@@ -14,12 +14,7 @@ public class API : IModApi
   {
     while (DespawnQueue.Count > 0)
     {
-      var entityId = DespawnQueue.Dequeue();
-
-      GameManager.Instance.World.RemoveEntity(entityId, EnumRemoveEntityReason.Despawned);
-
-      //var message = $"DROP-MINE: Removing {entityId}.";
-      //Log.Out(message);
+      GameManager.Instance.World.RemoveEntity(DespawnQueue.Dequeue(), EnumRemoveEntityReason.Despawned);
     }
   }
 
@@ -32,23 +27,7 @@ public class API : IModApi
   {
     if (entity is EntityFallingBlock)
     {
-      //var message = $"DROP-MINE: Queuing ({entity.entityId}) @ {entity.position} for despawn.";
-      //Log.Out(message);
-
       DespawnQueue.Enqueue(entity.entityId);
-
-      return;
-    }
-
-    if (EntityClass.list.TryGetValue(entity.entityClass, out var entityClass))
-    {
-      var entityCount = GameManager.Instance.World.Entities.Count;
-      var message = $"DROP-MINE: Entity Spawned {entityClass.entityClassName}({entity.entityId}) @ {entity.position} - Total: {entityCount}";
-
-      Log.Out(message);
     }
   }
-
-  //private static void SendGlobal(string message) =>
-  //  GameManager.Instance.ChatMessageServer(null, EChatType.Global, -1, message, "Server", false, null);
 }
